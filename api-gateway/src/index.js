1ddd02b8fb2ae96a8a2fd0f8db3bde5c4430be23
Neksyq <neksandrianov@gmail.com>
2024-09-29 14:40:27 +0200

    Implemented initial api get away, which now includes only prescription service

---
+++ api-gateway/src/index.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("combined"));
+
+const apiProxy = (path, target) => {
+  console.log()
+  app.use(
+    path,
+    createProxyMiddleware({
+      target,
+      changeOrigin: true,
+      pathRewrite: (pathReq, req) => pathReq.replace(path, ""),
+      onError: (err, req, res) => {
+        res.status(500).json({ error: "Proxy error", details: err.message });
+      },
+    })
+  );
+};
+
+
+// Routes and their target services
+// apiProxy("/api/auth", process.env.AUTH_SERVICE_URL);
+apiProxy("/api/prescriptions", process.env.PRESCRIPTION_SERVICE_URL);
+// apiProxy("/api/fulfillment", process.env.FULFILLMENT_SERVICE_URL);
+// apiProxy("/api/delivery", process.env.DELIVERY_SERVICE_URL);
+
+// Health Check Endpoint
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "API Gateway is running." });
+});
+
+// Start the server
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`API Gateway is running on port ${PORT}`);
+});
