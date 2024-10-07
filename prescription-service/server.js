98a533e7b2737bbc43f458224edfdf2aea1a8e58
Neksyq <neksandrianov@gmail.com>
2024-10-07 09:55:21 +0200

    Added middleware to handle general errors

--- prescription-service/server.js
+++ prescription-service/server.js
@@ -1,16 +1,19 @@
 const express = require("express");
-const app = express();
 const dotenv = require("dotenv");
 const prescriptionRoutes = require("./routes/prescriptions.js");
+const errorMiddleware = require('./middleware/errorMiddleware');
 
 dotenv.config();
 
+const app = express();
+const PORT = process.env.PRESCRIPTION_PORT;
+
 app.use(express.json());
-
 app.use(prescriptionRoutes);
+app.use(errorMiddleware); // Error handling middleware
 
-app.listen(process.env.PRESCRIPTION_PORT, () => {
+app.listen(PORT, () => {
   console.log(
-    `Prescription Service running on port ${process.env.PRESCRIPTION_PORT}`
+    `Prescription Service running on port ${PORT}`
   );
 });
